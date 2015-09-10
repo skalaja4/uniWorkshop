9e206534f949728de9c60c6b59c515b704d8c546
Student <petr.kupka@unicornsystems.cz>
2015-09-10 17:20:21 +0200

    Enities

---
+++ workshop/src/main/java/eu/unicorneducation/entity/Employee.java
@@ -0,0 +1,29 @@
+package eu.unicorneducation.entity;
+
+import java.util.Date;
+
+public  abstract class Employee {
+	
+	protected String  firstName ;
+	
+	protected String lastName;
+	
+	protected Date birthDate;
+	
+	protected BaPoCategory kind;
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
