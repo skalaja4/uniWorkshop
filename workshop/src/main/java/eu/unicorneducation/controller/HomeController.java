7f9657df7bf20105073607988f4d1e917e64994d
David Novotný <novotdav@gmail.com>
2015-09-10 16:32:41 +0200

    index.jsp

---
+++ workshop/src/main/java/eu/unicorneducation/controller/HomeController.java
@@ -0,0 +1,34 @@
+package eu.unicorneducation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String students(ModelMap model) {
+		
+//		model.addAttribute("studentsList", studentsFacade.findAll());
+		
+		return "index";
+	}
+	
+//	@RequestMapping(value = "/student", method = RequestMethod.GET)
+//	public ModelAndView students() {
+//		return new ModelAndView("new_student", "command", new StudentModel());
+//	}
+//	
+//	@RequestMapping(value = "/student", method = RequestMethod.POST)
+//	public String addStudent(@ModelAttribute("SpringWeb")StudentModel student, ModelMap model) {
+//		studentsFacade.save(student);
+//		return students(model);
+//	}
+	
+}
--- workshop/src/main/webapp/WEB-INF/index.jsp
+++ workshop/src/main/webapp/WEB-INF/index.jsp
@@ -1,5 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
 <html>
+<style type="text/css">
+	form {display: inline;}
+	input {font-size:40px;}
+</style>
+<head>
+	<title>Index</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+	<form action="http://google.com">
+		<input type="submit" value="Home">
+	</form>
+	<form action="http://centrum.cz">
+		<input type="submit" value="Import">
+	</form>
+	<form action="http://seznam.cz">
+		<input type="submit" value="Pobočky">
+	</form>
+	<form action="http://idnes.cz">
+		<input type="submit" value="Hodnocení">
+	</form>
 </body>
 </html>
