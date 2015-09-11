4ae3514e238e4a7e5e5ed29e5c156060ec6b748b
Skala <jaraskala@seznam.cz>
2015-09-11 15:38:25 +0200

    dao cele

--- workshop/src/main/java/eu/unicorneducation/dao/impl/EvaluationDAOImpl.java
+++ workshop/src/main/java/eu/unicorneducation/dao/impl/EvaluationDAOImpl.java
@@ -1,5 +1,41 @@
 package eu.unicorneducation.dao.impl;
 
-public class EvaluationDAOImpl {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import eu.unicorneducation.dao.EvaluationDAO;
+import eu.unicorneducation.entity.Employee;
+import eu.unicorneducation.entity.Evaluation;
+
+public class EvaluationDAOImpl implements EvaluationDAO {
+	private EntityManager em;
+
+	public EvaluationDAOImpl() {
+		em = Persistence.createEntityManagerFactory("workshop")
+				.createEntityManager();
+	}
+
+	public boolean create(Evaluation evaluation) {
+		em.getTransaction().begin();
+		em.persist(evaluation);
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public Evaluation read(Long id) {
+		return em.find(Evaluation.class, id);
+	}
+
+	public boolean delete(Evaluation evaluation) {
+		em.getTransaction().begin();
+		em.remove(evaluation);
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public List<Evaluation> readAll() {
+		return em.createQuery("SELECT e FROM Evaluation e",Evaluation.class).getResultList();
+	}
 }
--- workshop/src/main/java/eu/unicorneducation/dao/impl/EvaluationPlanDAOImpl.java
+++ workshop/src/main/java/eu/unicorneducation/dao/impl/EvaluationPlanDAOImpl.java
@@ -1,5 +1,44 @@
 package eu.unicorneducation.dao.impl;
 
-public class EvaluationPlanDAOImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import eu.unicorneducation.dao.EvaluationPlanDAO;
+import eu.unicorneducation.entity.Evaluation;
+import eu.unicorneducation.entity.EvaluationPlan;
+
+public class EvaluationPlanDAOImpl implements EvaluationPlanDAO {
+
+	private EntityManager em;
+
+	public EvaluationPlanDAOImpl() {
+		em = Persistence.createEntityManagerFactory("workshop")
+				.createEntityManager();
+	}
+
+	public boolean create(EvaluationPlan evaluationPlan) {
+		em.getTransaction().begin();
+		em.persist(evaluationPlan);
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public EvaluationPlan read(Long id) {
+		
+		return em.find(EvaluationPlan.class, id);
+	}
+
+	public boolean delete(EvaluationPlan evaluationPlan) {
+		em.getTransaction().begin();
+		em.remove(evaluationPlan);
+		em.getTransaction().commit();
+		return true;
+	}
+
+	public List<EvaluationPlan> readAll() {
+		return em.createQuery("SELECT e FROM EvaluationPlan e",EvaluationPlan.class).getResultList();
+	}
 
 }
