05d990168d6c4079b38c61233ff3549e054bb160
Ondrej <ondrej.flajsman@gmail.com>
2015-09-11 14:59:36 +0200

    Pridani addEvaluation.

--- workshop/src/main/java/eu/unicorneducation/controller/HomeController.java
+++ workshop/src/main/java/eu/unicorneducation/controller/HomeController.java
@@ -2,12 +2,16 @@ package eu.unicorneducation.controller;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import eu.unicorneducation.model.BranchModel;
 
 @Controller
 @RequestMapping("/")
@@ -73,6 +77,37 @@ public class HomeController {
 
 		return "plannedEvaluation";
 	}
+	
+	
+	@RequestMapping(value = "/addEvaluation", method = RequestMethod.GET)
+	public String addEvaluation(ModelMap model) {
+
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("src/main/webapp/WEB-INF/addEvaluation.jsp"));
+		} catch (IOException e) {
+			System.err.println(e.getStackTrace());
+		}
+
+		List<BranchModel> branches = new ArrayList<BranchModel>();
+		BranchModel b = new BranchModel();
+		b.setId("EU_matka");
+		b.setName("EU-Praha");
+		b.setAddress("Na Pøíkopech 1, Praha");
+		
+		BranchModel p = new BranchModel();
+		p.setId("EU_dcera");
+		p.setName("EU-Dcera");
+		p.setAddress("Botanická 2, Brno");
+		
+		branches.add(b);
+		branches.add(p);
+		
+		model.addAttribute("branches",branches);
+		model.addAttribute("properties", prop);
+		return "addEvaluation";
+	}
+	
 	@RequestMapping(value = "/fillEvaluation", method = RequestMethod.GET)
 	public String fillEvaluation(ModelMap model) {
 
