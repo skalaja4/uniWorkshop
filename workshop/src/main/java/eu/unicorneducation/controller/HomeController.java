b7aebb56439984b19a75ebe951df2d6ec18ceb29
David Novotný <novotdav@gmail.com>
2015-09-11 13:31:01 +0200

    test properties

--- workshop/src/main/java/eu/unicorneducation/controller/HomeController.java
+++ workshop/src/main/java/eu/unicorneducation/controller/HomeController.java
@@ -1,40 +1,52 @@
 package eu.unicorneducation.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String students(ModelMap model) {
-		
-//		model.addAttribute("studentsList", studentsFacade.findAll());
-		
+
+		// model.addAttribute("studentsList", studentsFacade.findAll());
+
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/inicialization", method = RequestMethod.GET)
-	public String inicialization() {
+	public String inicialization(ModelMap model) {
+
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("properties/inicializationTexts.config"));
+		} catch (IOException e) {
+			System.err.println(e.getStackTrace());
+		}
+
+		model.addAttribute("properties", prop);
 		return "inicialization";
 	}
-	
-//	@RequestMapping(value = "/student", method = RequestMethod.GET)
-//	public ModelAndView students() {
-//		return new ModelAndView("new_student", "command", new StudentModel());
-//	}
-//	
-//	@RequestMapping(value = "/student", method = RequestMethod.POST)
-//	public String addStudent(@ModelAttribute("SpringWeb")StudentModel student, ModelMap model) {
-//		studentsFacade.save(student);
-//		return students(model);
-//	}
+
+	// @RequestMapping(value = "/student", method = RequestMethod.GET)
+	// public ModelAndView students() {
+	// return new ModelAndView("new_student", "command", new StudentModel());
+	// }
+	//
+	// @RequestMapping(value = "/student", method = RequestMethod.POST)
+	// public String addStudent(@ModelAttribute("SpringWeb")StudentModel
+	// student, ModelMap model) {
+	// studentsFacade.save(student);
+	// return students(model);
+	// }
 	@RequestMapping(value = "/branches/", method = RequestMethod.GET)
 	public String branches() {
 		return "branches";
