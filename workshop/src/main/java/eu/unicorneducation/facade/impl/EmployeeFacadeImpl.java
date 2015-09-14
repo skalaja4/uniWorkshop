e86af521d34e2f23b0abcbee6cb91bac27ead140
Student <petr.kupka@unicornsystems.cz>
2015-09-14 14:58:08 +0200

    facade emp

--- workshop/src/main/java/eu/unicorneducation/facade/impl/EmployeeFacadeImpl.java
+++ workshop/src/main/java/eu/unicorneducation/facade/impl/EmployeeFacadeImpl.java
@@ -1,13 +1,16 @@
 package eu.unicorneducation.facade.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import eu.unicorneducation.entity.Employee;
+import eu.unicorneducation.entity.Evaluation;
 import eu.unicorneducation.facade.EmployeeFacade;
 import eu.unicorneducation.model.EmployeeModel;
+import eu.unicorneducation.model.EvaluationModel;
 import eu.unicorneducation.model.transformation.EmployeeTransformation;
 import eu.unicorneducation.service.EmployeeService;
 
@@ -57,6 +60,20 @@ public class EmployeeFacadeImpl implements EmployeeFacade {
 	public EmployeeModel readByID(String employeeId) {
 		Employee emp = employeeserv.readByID(employeeId);
 		return EmployeeTransformation.transToModel(emp);
+	}
+
+	@Override
+	public List<EvaluationModel> getEvaluations(String empID) {
+		List<EvaluationModel> listofev = new ArrayList<EvaluationModel>();
+		for(Evaluation e : employeeserv.getEvaluations(empID))
+		{
+			listofev.add(new EvaluationModel(e.getId(),e.getName(),e.getFill_date(),e.getQuestion1(),
+					e.getQuestion2(),e.getQuestion3(),e.getQuestion4(),e.getQuestion5(),e.getQuestion6(),
+					e.getQuestion7(),e.getQuestion8(),e.getQuestion9(),e.getQuestion10()));
+			
+		}
+		
+		return listofev;
 	}
 
 }
