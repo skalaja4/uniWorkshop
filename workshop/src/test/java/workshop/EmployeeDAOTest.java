a0b47d45c577b02ee0bb229d1563ce1c7ff8db69
Student <petr.kupka@unicornsystems.cz>
2015-09-16 10:05:46 +0200

    test

--- workshop/src/test/java/workshop/EmployeeDAOTest.java
+++ workshop/src/test/java/workshop/EmployeeDAOTest.java
@@ -1,11 +1,6 @@
 package workshop;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,12 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import eu.unicorneducation.dao.BranchDAO;
 import eu.unicorneducation.dao.EmployeeDAO;
-import eu.unicorneducation.dao.EvaluationPlanDAO;
-import eu.unicorneducation.dao.impl.BranchDAOImpl;
 import eu.unicorneducation.entity.Branch;
 import eu.unicorneducation.entity.Employee;
-import eu.unicorneducation.entity.EvaluationPlan;
-import eu.unicorneducation.enumeration.Category;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:beans-test.xml")
 public class EmployeeDAOTest {
@@ -31,12 +22,12 @@ public class EmployeeDAOTest {
 	@Autowired
 	BranchDAO branchDAO;
 	@Autowired
-	EmployeeDAO employeeDAO;
+	EmployeeDAO emplDAO;
 	
 	@Before
 	public void beforeTest(){
 		
-		Branch branch = new Branch("id1","name","address",null);
+		Branch branch = new Branch("BranchTest","Test","Testaddress",null);
 		branchDAO.create(branch);
 		
 	}
@@ -46,13 +37,13 @@ public class EmployeeDAOTest {
 		@Test
 		public void createDeletTest() {		
 			
-			Branch branch = branchDAO.read("id1");
-			Employee employee = new Employee("EMP_10001","Josef","Vomacka",branch,Calendar.getInstance().getTime(),eu.unicorneducation.enumeration.Category.NEW);
-			employeeDAO.create(employee);
+			Branch branch = branchDAO.read("BranchTest");
+			Employee employee = new Employee("EMP_1000","Josef","Vomacka",branch,Calendar.getInstance().getTime(),eu.unicorneducation.enumeration.Category.NEW);
+			emplDAO.create(employee);
 			
-			Assert.assertTrue(employeeDAO.create(employee));
+			Assert.assertTrue(emplDAO.create(employee));
 			
-			Assert.assertTrue(employeeDAO.delete(employee));
+			Assert.assertTrue(emplDAO.delete(employee));
 			branchDAO.delete(branch);
 
 		}
